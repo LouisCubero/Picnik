3e97621b5773806ba5aee063589b7e3bb0f88850
Cameron Wills <cameronscottwills@gmail.com>
2024-03-17 01:58:55 -0400

    feat: add mock upload page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  function createGroup() {
+    router.push("/upload");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-black">
@@ -12,7 +20,7 @@ export default function Home() {
       </h1>
       <p className="text-lg">Live in the moment with PicNick.</p>
       <div className="flex space-x-4">
-        <Button>
+        <Button onClick={createGroup}>
           <Plus className="mr-2 h-4 w-4" />
           Create Group
         </Button>
---
+++ app/upload/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Download, Upload, UserPlus } from "lucide-react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-black">
+        Pic
+        <br />
+        Nick
+      </h1>
+      <p className="text-lg">Upload your photos</p>
+      <div className="relative">
+        <img
+          className="rounded-lg m-4"
+          src="https://static.wikia.nocookie.net/silly-cat/images/4/4d/Mr._Fresh.png/"
+          width={360}
+          height={360}
+          alt={"Mr. Fresh Cat"}
+        />
+        <Button className="absolute bottom-8 right-8">
+          <Download className="h-4 w-4" />
+        </Button>
+      </div>
+      <div className="flex space-x-4">
+        <Button>
+          <UserPlus className="mr-2 h-4 w-4" />
+          Invite to Group
+        </Button>
+        <Button>
+          <Upload className="h-4 w-4" />
+        </Button>
+      </div>
+    </main>
+  );
+}
