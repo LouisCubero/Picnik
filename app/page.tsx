da7fe15dad91f03175c53000a382ef660b8c2604
Cameron Wills <cameronscottwills@gmail.com>
2024-03-17 00:32:07 -0400

    chore: formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { Plus } from "lucide-react"
+import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-black">Pic<br/>Nick</h1>
-      <p className="text-lg">
-        Live in the moment with PicNick.
-      </p>
+      <h1 className="text-6xl font-black">
+        Pic
+        <br />
+        Nick
+      </h1>
+      <p className="text-lg">Live in the moment with PicNick.</p>
       <div className="flex space-x-4">
-        <Button variant="default"><Plus className="mr-2 h-4 w-4" />Create Group</Button>
+        <Button>
+          <Plus className="mr-2 h-4 w-4" />
+          Create Group
+        </Button>
       </div>
     </main>
   );
